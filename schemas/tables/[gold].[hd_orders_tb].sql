d526bfd35dff58bb2338d975ceab700f3d311fc6
Atef Adel <atefadel1996123@gmail.com>
2026-02-26 15:50:01 +0200

    Create [gold].[hd_orders_tb].sql

---
+++ schemas/tables/[gold].[hd_orders_tb].sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS gold.fact_hd_orders
+CREATE TABLE gold.fact_hd_orders(
+ 	OrderId bigint NOT NULL,
+	OrderDate datetime NOT NULL,
+	CustomerNo int NOT NULL,
+	CustomerName nvarchar(150) NOT NULL,
+	StatusName nvarchar(100) NOT NULL,
+	TakerName nvarchar(150) NOT NULL,
+	Notes nvarchar(1000) NOT NULL,
+	CancelReasonName nvarchar(150) NOT NULL,
+	CustomerNotes nvarchar(1000) NOT NULL ,
+	OrderBranch INT NOT NULL,
+	TotalValue decimal(16, 3) NOT NULL,
+)
