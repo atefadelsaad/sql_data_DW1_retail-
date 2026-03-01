635f0b86615a0286d8e6e9dc16f5f89a342607af
Atef Adel <atefadel1996123@gmail.com>
2026-03-01 15:26:54 +0200

    Create [gold].[hd_oreder_items].sql

---
+++ schemas/tables/[gold].[hd_oreder_items].sql
@@ -0,0 +1,17 @@
+	DROP TABLE IF EXISTS gold.fact_hd_order_items
+	CREATE TABLE gold.fact_hd_order_items(
+		OrderId BIGINT NOT NULL,
+		OrderDate DATE NOT NULL,
+		DocType INT NOT NULL,
+		ItemEan NVARCHAR(20) NOT NULL,
+		Barcode NVARCHAR(20) NOT NULL,
+		ItemName NVARCHAR(150) NOT NULL,
+		Qty DECIMAL(12,3) NOT NULL,
+		ItemCount INT NOT NULL,
+		RetailPrice DECIMAL(20,3) NOT NULL,
+		DiscountValue DECIMAL(20,3) NOT NULL,
+		TotalValue DECIMAL(20,3) NOT NULL,
+		ItemClassName NVARCHAR(100) NOT NULL,
+		Notes NVARCHAR(1000) NOT NULL,
+		last_date DATETIME NOT NULL
+	)
