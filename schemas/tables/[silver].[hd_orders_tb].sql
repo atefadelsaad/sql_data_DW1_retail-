f0a4bcb6e28d6f92cca054777933475fad706f0c
Atef Adel <atefadel1996123@gmail.com>
2026-03-02 20:39:50 +0200

    Update [silver].[hd_orders_tb].sql

--- schemas/tables/[silver].[hd_orders_tb].sql
+++ schemas/tables/[silver].[hd_orders_tb].sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS silver.dim_hd_orders
 CREATE TABLE silver.dim_hd_orders(
  	OrderId bigint primary key NOT NULL,
-	OrderDate datetime NOT NULL,
+	OrderDate date NOT NULL,
 	CustomerNo int NOT NULL,
 	CustomerName nvarchar(150) NOT NULL,
 	StatusName nvarchar(100) NOT NULL,
