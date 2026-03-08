2c867e1b67a635e92e35f4e077771fc3a35bd168
Atef Adel <atefadel1996123@gmail.com>
2026-03-08 11:13:24 +0200

    Create [gold].[fact_dh_orders_sales_month].sql

---
+++ schemas/tables/[gold].[fact_dh_orders_sales_month].sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS gold.fact_dh_orders_sales_month
+CREATE TABLE gold.fact_dh_orders_sales_month(
+	Years INT NOT NULL,
+	months INT NOT NULL,
+	branchname NVARCHAR(50) NOT NULL,
+	total_orders BIGINT NOT NULL,
+	total_sales BIGINT NOT NULL,
+	Avg_Total BIGINT NOT NULL,
+	sales_status NVARCHAR(20) NOT NULL,
+	months_sales BIGINT  NULL,
+	last_update DATETIME NOT NULL
+)
